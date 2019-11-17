9fbbdada4f9f4bb1ca9791e05b961e48e089e62e
Jeretc79 <masonjeretc@gmail.com>
2019-11-16 21:31:40 -0800

    updated homework4

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var questions = [
-{
-prompt: ""
+var rightAnswers= ["11","Yes", 12, "All of the above","Yes/No"];
 
+
+
+function submitQuiz(that) {
+ var form = document.forms;
+ var firstQ = that.elements.q1;
+ for(var i=0; i <= firstQ.length; i++) {
+  if(firstQ[i].checked) {
+   // debugger;
+    if(firstQ[i].value == rightAnswers[0]){
+      alert("right answer!");
+    }
+  }
+
+ }
+
+ var firstQ = that.elements.q2;
+ var firstQ = that.elements.q3;
+ var firstQ = that.elements.q4;
+ var firstQ = that.elements.q5;
+ debugger;
+ alert(that);
+ //console.log("your score");
 }
 
-]
+
