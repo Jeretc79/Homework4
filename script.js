ae33f019b04b9adfaae2afc7ea8c5a778a5c340e
Jeretc79 <masonjeretc@gmail.com>
2019-11-19 20:11:41 -0800

    updated homework

--- script.js
+++ script.js
@@ -16,9 +16,34 @@ function submitQuiz(that) {
  }
 
  var firstQ = that.elements.q2;
+ for(var i=0; i <= firstQ.length; i++) {
+  if(firstQ[i].checked) {
+   // debugger;
+    if(firstQ[i].value == rightAnswers[0]){
+      alert("right answer!");
+    }
+  }
  var firstQ = that.elements.q3;
+ for(var i=0; i <= firstQ.length; i++) {
+  if(firstQ[i].checked) {
+    if(firstQ[i].value == rightAnswers[0]){
+      alert("right answer!");
+    }
+  }
  var firstQ = that.elements.q4;
+ for(var i=0; i <= firstQ.length; i++) {
+  if(firstQ[i].checked) {
+    if(firstQ[i].value == rightAnswers[0]){
+      alert("right answer!");
+    }
+  }
  var firstQ = that.elements.q5;
+ for(var i=0; i <= firstQ.length; i++) {
+  if(firstQ[i].checked) {
+    if(firstQ[i].value == rightAnswers[0]){
+      alert("right answer!");
+    }
+  }
  debugger;
  alert(that);
  //console.log("your score");
